ce86d5ff60bbee6b678ccade7eab1d2b79aeadcd
Tanel Ismael <tanel.ismael@khk.ee>
2019-10-07 13:48:44 +0300

    fix

--- src/GradesPoints.java
+++ src/GradesPoints.java
@@ -9,17 +9,17 @@ public class GradesPoints {
 
         System.out.println("Type the points [0-60]: " + points);
 
-        if ( (points <= 29) & (points >= 0)  ) {
+        if (points <= 29 && points >= 0) {
             System.out.println("\nGrade: failed");
-        } else if ((points >= 30 ) & (points <= 34)) {
+        } else if (points >= 30 && points <= 34) {
             System.out.println("\nGrade: 1");
-        } else if ((points >= 35 ) & (points <= 39)) {
+        } else if (points >= 35 && points <= 39) {
             System.out.println("\nGrade: 2");
-        } else if ((points >= 40 ) & (points <= 44)) {
+        } else if (points >= 40 && points <= 44) {
             System.out.println("\nGrade: 3");
-        } else if ((points >= 45 ) & (points <= 49)) {
+        } else if (points >= 45 && points <= 49) {
             System.out.println("\nGrade: 4");
-        } else if ((points >= 50 ) & (points <= 60)) {
+        } else if (points >= 50 && points <= 60) {
             System.out.println("\nGrade: 5");
         } else {
             System.out.println("\nNot a valid grade point!");
