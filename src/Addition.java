97a82e78576f27c14b0c4e832b880f1118a05353
Tanel Ismael <tanel.ismael@khk.ee>
2019-10-07 11:25:19 +0300

    addition and multiplication

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+public class Addition {
+    public static void main(String[] args) {
+        int firstNumber = 5;
+        int secondNumber = 4;
+        int sum = firstNumber + secondNumber;
+
+        System.out.println("5 + 4 = " + sum);
+
+
+
+
+
+    }
+}
---
+++ src/Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int firstNumber = 2;
+        int secondNumber = 8;
+        int sum = firstNumber * secondNumber;
+
+        System.out.println("2 * 8 = " + sum);
+
+
+    }
+}
